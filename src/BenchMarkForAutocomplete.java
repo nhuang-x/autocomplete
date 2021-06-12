15a373602a02300aaad1fe479c1e2a1bc2c83313
Nathan Huang <nathan.huang99@gmail.com>
2021-06-12 02:15:10 -0700

    Added local repo

--- src/BenchMarkForAutocomplete.java
+++ src/BenchMarkForAutocomplete.java
@@ -49,7 +49,7 @@ public class BenchMarkForAutocomplete {
 	}
 	
 	public void runAM() {
-		int matchSize = 50;
+		int matchSize = 10000;
 		String[] all = {"","", "a", "a", "b", "c", "g", "ga", "go", "gu", "x", "y", "z", "aa", "az", "za", "zz","zqzqwwx"};
 		System.out.printf("search\tsize\t#match");
 		for(int k=0; k < myCompletors.length; k++) {
@@ -99,7 +99,7 @@ public class BenchMarkForAutocomplete {
 	public void doMark() throws FileNotFoundException {
 		String fname = "data/threeletterwords.txt"; 
 		//fname = "data/fourletterwords.txt";
-		//fname = "data/alexa.txt";
+		fname = "data/alexa.txt";
 		setUp(fname);
 		runAM();
 	}
